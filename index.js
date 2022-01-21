d962f33360ca2db60c1492c39e2345a9cf6966b6
Shynet <shynet@gmail.com>
2022-01-21 16:13:08 +0200

    feature(clock): add time print when app is loaded

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const process = require('process');
 
 const animals = require('./animal_sounds.js');
 
+function printTime() {
+  console.log('-- The time now is: ' + new Date().toTimeString() + '--');
+}
+
 function main() {
   console.log('Main code running here');
+  printTime();
   console.log('-- This is a development version! --');
 
   const animalName = process.argv.at(2);
